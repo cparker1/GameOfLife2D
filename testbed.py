84ee8df6eeb2baf1ac3150815a50ffe212d0bd16
cparker1 <charles.andrew.parker@gmail.com>
2020-08-06 21:06:13 -0600

    Adding totally unnecessary camera movement to show 3d space in which this is rendered. Added debug info when F3 is pressed.

--- testbed.py
+++ testbed.py
@@ -1,4 +1,5 @@
 from direct.showbase.ShowBase import ShowBase
+from panda3d.core import WindowProperties
 from panda3d.core import LPoint3, LVector3
 from direct.gui.OnscreenText import OnscreenText
 from panda3d.core import TextNode, TransparencyAttrib
@@ -57,6 +58,12 @@ def loadObject(tex=None, pos=LPoint3(0, 0), depth=SPRITE_POS, scale=1,
 
     return obj
 
+# Function to put instructions on the screen.
+def addInstructions(pos, msg):
+    return OnscreenText(text=msg, style=1, fg=(1, 1, 1, 1), scale=.07,
+                        shadow=(0, 0, 0, 1), parent=base.a2dTopLeft,
+                        pos=(0.08, -pos - 0.04), align=TextNode.ALeft)
+
 
 class GameView(ShowBase):
 
@@ -64,7 +71,7 @@ class GameView(ShowBase):
         ShowBase.__init__(self)
 
         # This code puts the standard title and instruction text on screen
-        self.title = OnscreenText(text="Testing",
+        self.title = OnscreenText(text="2D Game of Life but it's 3D",
                                   parent=base.a2dBottomRight, scale=.07,
                                   align=TextNode.ARight, pos=(-0.1, 0.1),
                                   fg=(1, 1, 1, 1), shadow=(0, 0, 0, 0.5))
@@ -78,10 +85,50 @@ class GameView(ShowBase):
         # Disable default mouse-based camera control.  This is a method on the
         # ShowBase class from which we inherit.
         self.disableMouse()
+        # Make the mouse invisible, turn off normal mouse controls
+        self.disableMouse()
+        props = WindowProperties()
+        props.setCursorHidden(True)
+        self.win.requestProperties(props)
+        self.camLens.setFov(60)
         self.accept("escape", sys.exit)
 
+        #Handle mouse input and movement
+        self.heading = 0
+        self.pitch = 0
+        self.mousex = 0
+        self.mousey = 0
+        self.last = 0
+        self.mousebtn = [0, 0, 0]
+        self.keyboardbtn = {"w": 0, "a": 0, "s": 0, "d": 0, "lcontrol" : 0, "space": 0}
+        self.accept("w", self.setKeyBtn, ["w", 1])
+        self.accept("w-up", self.setKeyBtn, ["w", 0])
+        self.accept("a", self.setKeyBtn, ["a", 1])
+        self.accept("a-up", self.setKeyBtn, ["a", 0])
+        self.accept("s", self.setKeyBtn, ["s", 1])
+        self.accept("s-up", self.setKeyBtn, ["s", 0])
+        self.accept("d", self.setKeyBtn, ["d", 1])
+        self.accept("d-up", self.setKeyBtn, ["d", 0])
+        self.accept("lcontrol", self.setKeyBtn, ["lcontrol", 1])
+        self.accept("lcontrol-up", self.setKeyBtn, ["lcontrol", 0])
+        self.accept("space", self.setKeyBtn, ["space", 1])
+        self.accept("space-up", self.setKeyBtn, ["space", 0])
+        self.accept("f3", self.toggleDebug, [])
+
+        #setup debug text
+        self.show_debug_text = False
+        self.dtext_xyz = addInstructions(0.1, "TEST")
+        self.dtext_heading = addInstructions(0.20, "TEST")
+        self.dtext_pitch = addInstructions(0.3, "TEST")
+        self.toggleDebug()
+        self.toggleDebug()
+
+        #Center the camera on the game grid
+        self.camera.setPos(0, 0, 0)
+        self.camera.setHpr(self.heading, self.pitch, 0)
+
         # Load the background color
-        # self.setBackgroundColor((50, 50, 50, 1))
+        # self.setBackgroundColor((0.65, 0.65, 0.65, 1))
 
         #test loading model, loader function/class is a part of ShowBase
         self.cell_sprites = []
@@ -95,14 +142,84 @@ class GameView(ShowBase):
                 self.cell_sprites.append([new_sprite, self.g.get_cell(h, w)])
 
 
-
-
         # Now we create the task. taskMgr is the task manager that actually
         # calls the function each frame. The add method creates a new task.
         # The first argument is the function to be called, and the second
         # argument is the name for the task.  It returns a task object which
         # is passed to the function each frame.
         self.gameTask = taskMgr.add(self.game_loop, "gameLoop")
+        self.cameraTask = taskMgr.add(self.controlCamera, "cameratask")
+
+    def setKeyBtn(self, btn_key, value):
+        self.keyboardbtn[btn_key] = value
+
+    def toggleDebug(self):
+        if self.show_debug_text:
+            self.show_debug_text = False
+            self.dtext_xyz.destroy()
+            self.dtext_heading.destroy()
+            self.dtext_pitch.destroy()
+        else:
+            self.show_debug_text = True
+
+    def drawDebug(self):
+        self.dtext_xyz.destroy()
+        self.dtext_heading.destroy()
+        self.dtext_pitch.destroy()
+        dtext_heading_text = "Heading: {:.2f}".format(self.heading)
+        dtext_pitch_text = "Pitch: {:.2f}".format(self.pitch)
+        dtext_xyz_text = "[X, Y, Z]: {:.2f}, {:.2f}, {:.2f}".format(self.camera.getX(),
+                                                        self.camera.getY(),
+                                                        self.camera.getZ())
+        self.dtext_xyz = addInstructions(0.07, dtext_xyz_text)
+        self.dtext_heading = addInstructions(0.14, dtext_heading_text)
+        self.dtext_pitch = addInstructions(0.21, dtext_pitch_text)
+
+
+    def controlCamera(self, task):
+        # figure out how much the mouse has moved (in pixels)
+        md = self.win.getPointer(0)
+        x = md.getX()
+        y = md.getY()
+        if self.win.movePointer(0, 100, 100):
+            self.heading = self.heading - (x - 100) * 0.2
+            self.pitch = self.pitch - (y - 100) * 0.2
+        if self.pitch < -45:
+            self.pitch = -45
+        if self.pitch > 45:
+            self.pitch = 45
+        self.camera.setHpr(self.heading, self.pitch, 0)
+
+        delta = globalClock.getDt()
+        move_x = delta * 30 * -self.keyboardbtn['a'] + delta * 30 * self.keyboardbtn['d']
+        move_y = delta * 30 * -self.keyboardbtn['lcontrol'] + delta * 30 * self.keyboardbtn['space']
+        move_z = delta * 30 * self.keyboardbtn['s'] + delta * 30 * -self.keyboardbtn['w']
+        self.camera.setPos(self.camera, move_x, -move_z, move_y)
+        self.camera.setHpr(self.heading, self.pitch, 0)
+
+        elapsed = task.time - self.last
+        if self.last == 0:
+            elapsed = 0
+        if self.camera.getX() < -59.0:
+            self.camera.setX(-59)
+        if self.camera.getX() > 59.0:
+            self.camera.setX(59)
+        if self.camera.getY() < -59.0:
+            self.camera.setY(-59)
+        if self.camera.getY() > 59.0:
+            self.camera.setY(59)
+        if self.camera.getZ() < -20.0:
+            self.camera.setZ(-20.0)
+        if self.camera.getZ() > 45.0:
+            self.camera.setZ(45)
+        # self.focus = self.camera.getPos() + (dir * 5)
+        self.last = task.time
+
+        if self.show_debug_text:
+            self.drawDebug()
+
+
+        return Task.cont
 
     def update_sprites(self):
         for cellpack in self.cell_sprites:
@@ -130,25 +247,6 @@ class GameView(ShowBase):
 
 
 if __name__ == "__main__":
-
     v = GameView()
     v.run()
-    # g = grid.LifeGrid2D(30, 13)
-    # grid.LifeGrid2D.sprinkle_life(g, 100)
-    # step_cnt = 0
-    #
-    # while True:
-    #     system("cls")
-    #     print("Ctrl+c to quit")
-    #     g.process_current_step()
-    #     g.update_to_next_step()
-    #     step_cnt += 1
-    #     g.print_grid()
-    #     time.sleep(0.5)
-    #     if g.was_there_change is False:
-    #         g.sprinkle_life(100)
-    #     elif step_cnt > 5:
-    #         g.sprinkle_life(30)
-    #         step_cnt = 0
-    #
-    #
+
